49d1a547860fbd5a5725b74326d8d119afc79de6
Oyudi22 <ochiaifelipe@gmail.com>
2025-03-31 18:52:31 -0300

    Sending the values to the server

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,9 +1,13 @@
+const Atendimento = require('../models/atendimentos');
+
 module.exports = app => {
     app.get("/atendimentos", (req, res) => res.send("Servidor funcionando, tudo ok!"));
 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body);
-        res.send('Você esta na rota de atendimento e está realizando um POST')
+        const atendimento = req.body;
+
+        Atendimento.adiciona(atendimento);
+        res.send('POST Atendimento')
     
     });
 }
