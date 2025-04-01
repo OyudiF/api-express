907173440aea4b38b2db535616e23b19d3d3d170
Oyudi22 <ochiaifelipe@gmail.com>
2025-04-01 18:50:57 -0300

    Error treatment

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -2,20 +2,44 @@ const moment = require('moment');
 const connection = require('../infraestrutura/connection');
 
 class Atendimento {
-    adiciona(atendimento) {
+    adiciona(atendimento, res) {
         const data_criacao = moment().format('YYYY-MM-DD HH:mm:ss');
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');
-        const atendimentoDatado = {...atendimento, data_criacao, data};
         
-        const sql = 'INSERT INTO Atendimentos SET ?';
+        const dataValida = moment(data).isSameOrAfter(data_criacao);
+        const clienteValido = atendimento.cliente.length >= 3;
 
-        connection.query(sql, atendimentoDatado, (erro, resultados) => {
-            if(erro){
-                console.log(erro);
-            } else{
-                console.log(resultados);
+        const validacoes = [
+            {
+                nome: data,
+                valido: dataValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: cliente,
+                valido: clienteValido,
+                mensagem: 'Cliente deve ter pelo menos tres caracteres'
             }
-        });
+        ];
+
+        const erros = validacoes.filter(campo => !campo.valido);
+        const errosExist = erros.length;
+
+        if(errosExist){
+            res.status(400).json(erros);
+        } else {    
+            const atendimentoDatado = {...atendimento, data_criacao, data};
+
+            const sql = 'INSERT INTO Atendimentos SET ?';
+
+            connection.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro){
+                    res.status(400).json(erro);
+                } else{
+                    res.status(201).json(resultados);
+                }
+            });
+        }
     }
 }
 
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
-## API com EXPRESS
+## API de Pet-shop com EXPRESS
 
 <h2>Essa uma API construída em EXPRESS com a utilização do node e demais módulos.</h2>
+
+<p>Essa API, cria no banco de dados o atendimento do serviço</p>
+<table>
+    <tr></tr>
+</table>
